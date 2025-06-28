504731231cfc6c0c2d485b8940505a7942197f34
Saeed Yamaguchi <42499925+yamaguchi-saeed@users.noreply.github.com>
2025-06-28 15:26:56 +0900

    Update Inputs.jsx, fixing errors

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+import React, { useState } from 'react';
+import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 import { toast } from 'react-toastify';
 
-
 function Inputs({ setQuery, units, setUnits }) {
-    const [city, setCity] = useState("")
+    const [city, setCity] = useState("");
 
     const handleUnitsChange = (e) => {
-        const selectedUnit = e.currentTarget.name
+        const selectedUnit = e.currentTarget.name;
         if (units !== selectedUnit) setUnits(selectedUnit);
     };
 
     const handleSearchClick = () => {
-        if (city !== '') setQuery({ q: city })
+        if (city !== '') setQuery({ q: city });
     };
 
     const handleLocationClick = () => {
         if (navigator.geolocation) {
             toast.info("Fetching user's location");
-            navigation.geolocation.getCurrentPosition((position) => {
+            navigator.geolocation.getCurrentPosition((position) => {
                 toast.success("Location fetched!");
                 let lat = position.coords.latitude;
                 let lon = position.coords.longitude;
@@ -26,11 +25,11 @@ function Inputs({ setQuery, units, setUnits }) {
                 setQuery({
                     lat,
                     lon,
-                })
-
-            })
+                });
+            });
         }
-    }
+    };
+
     return (
         <div className='flex flex-row justify-center my-6'>
             <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
@@ -53,24 +52,27 @@ function Inputs({ setQuery, units, setUnits }) {
                     className="text-white cursor-pointer transition ease-out hover:scale-125"
                     onClick={handleLocationClick}
                 />
-
-
             </div>
 
             <div className="flex flex-row w-1/4 items-center justify-center">
-                <button name="metric" className="text-xl text-white font-light transition ease-out hover: scale-125" onClick={handleUnitsChange}>
+                <button
+                    name="metric"
+                    className="text-xl text-white font-light transition ease-out hover:scale-125"
+                    onClick={handleUnitsChange}
+                >
                     °C
                 </button>
-                <p className="text-xl text-white mx-1">
-                    |
-                </p>
-                <button name="imperial" className="text-xl text-white font-light transition ease-out hover: scale-125 " onClick={handleUnitsChange}>
+                <p className="text-xl text-white mx-1">|</p>
+                <button
+                    name="imperial"
+                    className="text-xl text-white font-light transition ease-out hover:scale-125"
+                    onClick={handleUnitsChange}
+                >
                     °F
                 </button>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Inputs
+export default Inputs;
