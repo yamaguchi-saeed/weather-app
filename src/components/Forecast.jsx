40e2c1dda1e09e7462a3eee6426ab39663806353
Saeed Yamaguchi <42499925+yamaguchi-saeed@users.noreply.github.com>
2025-06-28 15:27:24 +0900

    Update Forecast.jsx, fixing errors

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { iconUrlFromCode } from '../services/weatherService'
+import React from 'react';
+import { iconUrlFromCode } from '../services/weatherService';
 
-function Forecast({ title }) {
+function Forecast({ title, items }) {
     return (
-
         <div>
             <div className='flex items-center justify-start my-6'>
                 <p className='text-white font-medium uppercase'>
@@ -12,29 +11,23 @@ function Forecast({ title }) {
             </div>
 
             <hr className='my-2' />
-            <div className='flex flex-row items-center justify-between text-white '>
-
-                {items.map(item => (
-                    <div className='flex flex-col items-center justify-center'>
-                        <p className='font-light text-sm'>
-
-                            {item.title}
-                        </p>
+            <div className='flex flex-row items-center justify-between text-white'>
+                {items.map((item, index) => (
+                    <div key={index} className='flex flex-col items-center justify-center'>
+                        <p className='font-light text-sm'>{item.title}</p>
                         <img
                             src={iconUrlFromCode(item.icon)}
-                            alt=""
+                            alt="weather icon"
                             className="w-20 my-1"
                         />
                         <p className='font-medium'>
-                            {`%{item.temp.toFixed()}°`}
+                            {`${item.temp.toFixed()}°`}
                         </p>
                     </div>
-                )
-                )
-                };
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default Forecast
+export default Forecast;
